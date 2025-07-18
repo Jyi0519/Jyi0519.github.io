8bdaef8aeaab18de026858d54ed659e95e6023e7
Jyi0519 <sjy_2023_0902@qq.com>
2025-07-18 17:51:37 +0800

    Site updated: 2025-07-18 17:51:37

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,18 @@
-// 使用主题内置的 jQuery，无需重新加载
-$(function() {
-    $("#loader-container").fadeOut(560);
+function loadScript(url, callback) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    script.onload = function() {
+        callback();
+    };
+
+    document.head.appendChild(script);
+}
+
+// 加载 jQuery（若主题已引入可省略）
+loadScript('https://code.jquery.com/jquery-3.6.0.min.js', function() {
+    $(function(){
+        $("#loader-container").fadeOut(560);
+    });
 });
