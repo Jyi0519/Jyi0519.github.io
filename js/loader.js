33b444f8f006aca51c17dc4577b072fa13a00833
Jyi0519 <sjy_2023_0902@qq.com>
2025-07-18 17:55:21 +0800

    Site updated: 2025-07-18 17:55:21

--- js/loader.js
+++ js/loader.js
@@ -1,18 +1,3 @@
-function loadScript(url, callback) {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-
-    script.onload = function() {
-        callback();
-    };
-
-    document.head.appendChild(script);
-}
-
-// 加载 jQuery（若主题已引入可省略）
-loadScript('https://code.jquery.com/jquery-3.6.0.min.js', function() {
-    $(function(){
-        $("#loader-container").fadeOut(560);
-    });
+$(function() {
+    $("#loader-container").fadeOut(560);
 });
